5a6ec6621fb47a31b011ff58120616a33abb65e4
Corey R. Stagner <cresc0428@gmail.com>
2017-08-06 19:52:11 -0400

    separated questions into JSON files and have working gameChoice(), startBasicTrivia(), askQuestion(), and pickCard()

--- basicCard.js
+++ basicCard.js
@@ -1,8 +1,26 @@
 console.log("basicCard.js was imported");
 
+var basicCardArr = [
+    {
+        question: 'Who was the first president of the united States?',
+        answer: 'George Washington' },
+    { 
+        question: 'What number comes after 1?', 
+        answer: '2' }
+]
+
 function BasicCard(question, answer) {
     this.question = question;
     this.answer = answer;
 }// end BasicCard({})
 
-module.export = BasicCard;
+function createQuestions(){
+    var card1 = new BasicCard("Who was the first president of the united States?", "George Washington");
+    var card2 = new BasicCard("What number comes after 1?", "2");
+    basicCardArr.push(card1);
+    basicCardArr.push(card2);
+} // end createQuestions()
+
+createQuestions();
+
+module.export = basicCardArr;
--- index.js
+++ index.js
@@ -11,11 +11,16 @@
 // Requires
 var gimme = require("inquirer");
 var chalk = require("chalk");
-var card = require("./basicCard.js");
-var cloze = require("./clozeCard.js");
+var cardConst = require("./basicCard.js");
+var cardLib = require("./basicCardLibrary.json");
+var clozeConst = require("./clozeCard.js");
+var clozeLib = require("./clozeCardLibrary.json");
+var colors = require("colors");
+
 
 // Initial Variables
 var humane = true;
+var score = 0;
 var questions = [];
 var humaneQuestions = ["Question 1", "Question 2", "Question 3"];
 var answers = ["Answer 1", "Answer 2","Answer 3","Answer 4","Answer 5","Answer 6"];
@@ -23,10 +28,17 @@ var inhumaneQuestions = ["aQuestion 1", "aQuestion 2", "aQuestion 3"];
 var aAnswers = ["aAnswer 1", "aAnswer 2","aAnswer 3","aAnswer 4","aAnswer 5","aAnswer 6"];
 var p1Hand = []; // holds player 1 cards so that they can choose one for the question
 
+/////////////////////////////////////////////////
 // Functions
+/////////////////////////////////////////////////
+
+// function pretty(input){
+//     JSON.stringify(input, null, 2);
+// }// end pretty()
 
 // Check to see if the player wants to play the clean version or also have a shot of whiskey with thier game
 function gameChoice(){
+    score = 0;
     gimme.prompt([
         {
             type: "list",
@@ -42,7 +54,7 @@ function gameChoice(){
             gimme.prompt([
                 {
                     type:"list",
-                    message: "Grab a drink, and lets play. Which game do you want to play?",
+                    message: "Grab a drink, and let's play. Which game do you want to play?",
                     choices: ["Standard Trivia Game", "Advanced Trivia Game", "Cards Against Humanity In a Terminal Interface"],
                     name: "game"
                 }// end prompt()
@@ -82,19 +94,65 @@ function gameChoice(){
                     } else{
                         console.log("I have no idea how you got here!");
                     }
-            })// end then()
+            });// end then()
         } else {
             console.log("Something is not working on my age prompt question!")
         }//end else if()
-    })// end gimme.prompt(Are you 21?)
+    });// end gimme.prompt(Are you 21?)
 }// end gameChoice()
+
+function pickCard(){
+    var questionsRemaining = questions.length;
+    var randomIndex = Math.floor(Math.random() * questionsRemaining);
+    var chosenCard = questions[randomIndex];
+    console.log(chosenCard);
+    questions.splice(randomIndex, 1);
+    return chosenCard;
+
+}// end pickCard()
+
+function askQuestion(){
+    if(questions.length > 0){
+        console.log("Game continues");
+        theCard = pickCard();
+        console.log(theCard.quesion);
+        console.log(theCard.answer);
+        console.log("Your Question is " + JSON.stringify(theCard, null, 2));
+        gimme.prompt([
+            {
+                type: "input",
+                message: theCard.front,
+                name: "question"
+            }
+            ]).then(function(response){
+                var answer = response.question;
+                if (answer.toLowerCase === theCard.back.toLowerCase){
+                    console.log("Correct");
+                } else {
+                    console.log("Wrong");
+                }
+                // recursion of function
+                askQuestion();
+            });// end gimme.prompt.then()
+    }else{
+        console.log("Game is Over")
+    }// end if/else()
+}
 
 function startBasicTrivia(){
     console.log("Started Basic Trivia Game");
-}
+    // Setup Basic Trivia Game
+    questions = cardLib;
+    var rounds = questions.length;
+    console.log("There will be " + rounds + " rounds.");
+    askQuestion();
+}// end startBasicTrivia()
 
 function startAdvancedTrivia(){
     console.log("Started Advanced Trivia Game");
+    // Setup Advanced Trivia Game
+    questions = clozeLib;
+    pickCard();
 }
 
 function startCAHITI() {
@@ -107,3 +165,46 @@ function chooseQuestion(options){
 }
 
 gameChoice();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function shuffleDeck() {
+  newDeck = library.slice(0); //copy the flashcards into a new array
+  for (var i = library.length - 1; i > 0; i--) { //this algorithm (Fisher-Yates shuffle) should jumble up the order of the copied array
+
+      var getIndex = Math.floor(Math.random() * (i + 1));
+      var shuffled = newDeck[getIndex];
+
+      newDeck[getIndex] = newDeck[i];
+
+      newDeck[i] = shuffled;
+  }
+  fs.writeFile("cardLibrary.json", JSON.stringify(newDeck, null, 2)); //write the new randomized array over the old one
+  console.log(colors.cyan("The deck of flashcards have been shuffled"));
+  //setTimeout(openMenu, 1000);  //*** shuffle only works on app reload, look into how to apply it in-app
+}
